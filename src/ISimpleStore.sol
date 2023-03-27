a7700c5df384d6863a090cc45670481c8badcc32
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2023-03-27 13:07:18 +0200

    Delete a bunch of stuff

--- src/ISimpleStore.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.13;
-
-interface ISimpleStore {
-    function store(uint256 val) external;
-
-    function get() external returns (uint256);
-}
