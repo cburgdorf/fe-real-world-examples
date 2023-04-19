786fb39e79477461452718987af6d113b2f40c8b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2023-04-19 13:01:51 +0200

    Basic confirmation

--- src/test/MultiSig.t.sol
+++ src/test/MultiSig.t.sol
@@ -73,8 +73,11 @@ contract MultiSigTest is Test {
       vm.startPrank(BINANCE_ACCOUNT);
       IERC20(DAI).transfer(address(multisig), 10000);
       uint256 initial_multisig_balance = IERC20(DAI).balanceOf(multisig_address);
+
+      vm.stopPrank();
+      vm.startPrank(0x627306090abaB3A6e1400e9345bC60c78a8BEf57);
       uint256 tx_id = multisig.add_transaction(DAI, 0, data, 68);
-      multisig.execute_transaction(tx_id);
+      multisig.confirm_transaction(tx_id);
 
       uint256 second_multisig_balance = IERC20(DAI).balanceOf(multisig_address);
       assertEq(second_multisig_balance, initial_multisig_balance - 1);
